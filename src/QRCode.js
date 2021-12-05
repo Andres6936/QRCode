ddc7ac58f0570dcb1c7eb29716420901aeae9f57
Andres6936 <joan.buritica@artemisa.tech>
2021-12-05 07:15:36 -0500

    Avoid IIF

--- src/QRCode.js
+++ src/QRCode.js
@@ -15,173 +15,167 @@ import {DrawingHack} from "./DrawerHack.js";
 import {_getAndroid, DrawingCanvas} from "./DrawingCanvas.js";
 import {DrawingSVG} from "./DrawingSVG.js";
 
-export var QRCode;
+function _isSupportCanvas() {
+    return typeof CanvasRenderingContext2D != "undefined";
+}
 
-(function () {
+var svgDrawer = DrawingSVG;
 
-
-    function _isSupportCanvas() {
-        return typeof CanvasRenderingContext2D != "undefined";
-    }
-
-    var svgDrawer = DrawingSVG;
-
-    var useSVG = document.documentElement.tagName.toLowerCase() === "svg";
+var useSVG = document.documentElement.tagName.toLowerCase() === "svg";
 
 // Drawing in DOM by using Table tag
-    var Drawing = useSVG ? svgDrawer : !_isSupportCanvas() ? DrawingHack : DrawingCanvas;
+var Drawing = useSVG ? svgDrawer : !_isSupportCanvas() ? DrawingHack : DrawingCanvas;
 
-    /**
-     * Get the type by string length
-     *
-     * @private
-     * @param {String} sText
-     * @param {Number} nCorrectLevel
-     * @return {Number} type
-     */
-    function _getTypeNumber(sText, nCorrectLevel) {
-        var nType = 1;
-        var length = _getUTF8Length(sText);
+/**
+ * Get the type by string length
+ *
+ * @private
+ * @param {String} sText
+ * @param {Number} nCorrectLevel
+ * @return {Number} type
+ */
+function _getTypeNumber(sText, nCorrectLevel) {
+    var nType = 1;
+    var length = _getUTF8Length(sText);
 
-        for (var i = 0, len = QRCodeLimitLength.length; i <= len; i++) {
-            var nLimit = 0;
+    for (var i = 0, len = QRCodeLimitLength.length; i <= len; i++) {
+        var nLimit = 0;
 
-            switch (nCorrectLevel) {
-                case QRErrorCorrectLevel.L :
-                    nLimit = QRCodeLimitLength[i][0];
-                    break;
-                case QRErrorCorrectLevel.M :
-                    nLimit = QRCodeLimitLength[i][1];
-                    break;
-                case QRErrorCorrectLevel.Q :
-                    nLimit = QRCodeLimitLength[i][2];
-                    break;
-                case QRErrorCorrectLevel.H :
-                    nLimit = QRCodeLimitLength[i][3];
-                    break;
-            }
-
-            if (length <= nLimit) {
+        switch (nCorrectLevel) {
+            case QRErrorCorrectLevel.L :
+                nLimit = QRCodeLimitLength[i][0];
                 break;
-            } else {
-                nType++;
-            }
+            case QRErrorCorrectLevel.M :
+                nLimit = QRCodeLimitLength[i][1];
+                break;
+            case QRErrorCorrectLevel.Q :
+                nLimit = QRCodeLimitLength[i][2];
+                break;
+            case QRErrorCorrectLevel.H :
+                nLimit = QRCodeLimitLength[i][3];
+                break;
         }
 
-        if (nType > QRCodeLimitLength.length) {
-            throw new Error("Too long data");
+        if (length <= nLimit) {
+            break;
+        } else {
+            nType++;
         }
-
-        return nType;
     }
 
-    function _getUTF8Length(sText) {
-        var replacedText = encodeURI(sText).toString().replace(/\%[0-9a-fA-F]{2}/g, 'a');
-        return replacedText.length + (replacedText.length != sText ? 3 : 0);
+    if (nType > QRCodeLimitLength.length) {
+        throw new Error("Too long data");
     }
 
-    /**
-     * @class QRCode
-     * @constructor
-     * @example
-     * new QRCode(document.getElementById("test"), "http://jindo.dev.naver.com/collie");
-     *
-     * @example
-     * var oQRCode = new QRCode("test", {
-     *    text : "http://naver.com",
-     *    width : 128,
-     *    height : 128
-     * });
-     *
-     * oQRCode.clear(); // Clear the QRCode.
-     * oQRCode.makeCode("http://map.naver.com"); // Re-create the QRCode.
-     *
-     * @param {HTMLElement|String} el target element or 'id' attribute of element.
-     * @param {Object|String} vOption
-     * @param {String} vOption.text QRCode link data
-     * @param {Number} [vOption.width=256]
-     * @param {Number} [vOption.height=256]
-     * @param {String} [vOption.colorDark="#000000"]
-     * @param {String} [vOption.colorLight="#ffffff"]
-     * @param {QRCode.CorrectLevel} [vOption.correctLevel=QRCode.CorrectLevel.H] [L|M|Q|H]
-     */
-    QRCode = function (el, vOption) {
-        this._htOption = {
-            width: 256,
-            height: 256,
-            typeNumber: 4,
-            colorDark: "#000000",
-            colorLight: "#ffffff",
-            correctLevel: QRErrorCorrectLevel.H
+    return nType;
+}
+
+function _getUTF8Length(sText) {
+    var replacedText = encodeURI(sText).toString().replace(/\%[0-9a-fA-F]{2}/g, 'a');
+    return replacedText.length + (replacedText.length != sText ? 3 : 0);
+}
+
+/**
+ * @class QRCode
+ * @constructor
+ * @example
+ * new QRCode(document.getElementById("test"), "http://jindo.dev.naver.com/collie");
+ *
+ * @example
+ * var oQRCode = new QRCode("test", {
+ *    text : "http://naver.com",
+ *    width : 128,
+ *    height : 128
+ * });
+ *
+ * oQRCode.clear(); // Clear the QRCode.
+ * oQRCode.makeCode("http://map.naver.com"); // Re-create the QRCode.
+ *
+ * @param {HTMLElement|String} el target element or 'id' attribute of element.
+ * @param {Object|String} vOption
+ * @param {String} vOption.text QRCode link data
+ * @param {Number} [vOption.width=256]
+ * @param {Number} [vOption.height=256]
+ * @param {String} [vOption.colorDark="#000000"]
+ * @param {String} [vOption.colorLight="#ffffff"]
+ * @param {QRCode.CorrectLevel} [vOption.correctLevel=QRCode.CorrectLevel.H] [L|M|Q|H]
+ */
+export var QRCode = function (el, vOption) {
+    this._htOption = {
+        width: 256,
+        height: 256,
+        typeNumber: 4,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRErrorCorrectLevel.H
+    };
+
+    if (typeof vOption === 'string') {
+        vOption = {
+            text: vOption
         };
+    }
 
-        if (typeof vOption === 'string') {
-            vOption = {
-                text: vOption
-            };
+    // Overwrites options
+    if (vOption) {
+        for (var i in vOption) {
+            this._htOption[i] = vOption[i];
         }
+    }
 
-        // Overwrites options
-        if (vOption) {
-            for (var i in vOption) {
-                this._htOption[i] = vOption[i];
-            }
-        }
+    if (typeof el == "string") {
+        el = document.getElementById(el);
+    }
 
-        if (typeof el == "string") {
-            el = document.getElementById(el);
-        }
+    if (this._htOption.useSVG) {
+        Drawing = svgDrawer;
+    }
 
-        if (this._htOption.useSVG) {
-            Drawing = svgDrawer;
-        }
+    this._android = _getAndroid();
+    this._el = el;
+    this._oQRCode = null;
+    this._oDrawing = new Drawing(this._el, this._htOption);
 
-        this._android = _getAndroid();
-        this._el = el;
-        this._oQRCode = null;
-        this._oDrawing = new Drawing(this._el, this._htOption);
+    if (this._htOption.text) {
+        this.makeCode(this._htOption.text);
+    }
+};
 
-        if (this._htOption.text) {
-            this.makeCode(this._htOption.text);
-        }
-    };
+/**
+ * Make the QRCode
+ *
+ * @param {String} sText link data
+ */
+QRCode.prototype.makeCode = function (sText) {
+    this._oQRCode = new QRCodeModel(_getTypeNumber(sText, this._htOption.correctLevel), this._htOption.correctLevel);
+    this._oQRCode.addData(sText);
+    this._oQRCode.make();
+    this._el.title = sText;
+    this._oDrawing.draw(this._oQRCode);
+    this.makeImage();
+};
 
-    /**
-     * Make the QRCode
-     *
-     * @param {String} sText link data
-     */
-    QRCode.prototype.makeCode = function (sText) {
-        this._oQRCode = new QRCodeModel(_getTypeNumber(sText, this._htOption.correctLevel), this._htOption.correctLevel);
-        this._oQRCode.addData(sText);
-        this._oQRCode.make();
-        this._el.title = sText;
-        this._oDrawing.draw(this._oQRCode);
-        this.makeImage();
-    };
+/**
+ * Make the Image from Canvas element
+ * - It occurs automatically
+ * - Android below 3 doesn't support Data-URI spec.
+ *
+ * @private
+ */
+QRCode.prototype.makeImage = function () {
+    if (typeof this._oDrawing.makeImage == "function" && (!this._android || this._android >= 3)) {
+        this._oDrawing.makeImage();
+    }
+};
 
-    /**
-     * Make the Image from Canvas element
-     * - It occurs automatically
-     * - Android below 3 doesn't support Data-URI spec.
-     *
-     * @private
-     */
-    QRCode.prototype.makeImage = function () {
-        if (typeof this._oDrawing.makeImage == "function" && (!this._android || this._android >= 3)) {
-            this._oDrawing.makeImage();
-        }
-    };
+/**
+ * Clear the QRCode
+ */
+QRCode.prototype.clear = function () {
+    this._oDrawing.clear();
+};
 
-    /**
-     * Clear the QRCode
-     */
-    QRCode.prototype.clear = function () {
-        this._oDrawing.clear();
-    };
-
-    /**
-     * @name QRCode.CorrectLevel
-     */
-    QRCode.CorrectLevel = QRErrorCorrectLevel;
-})();
+/**
+ * @name QRCode.CorrectLevel
+ */
+QRCode.CorrectLevel = QRErrorCorrectLevel;
