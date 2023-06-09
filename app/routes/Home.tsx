ab1e14780be95b30be474f580f6629e3b899c178
Andres6936 <andres6936@gmail.com>
2023-06-09 09:37:52 -0500

    Prepare Code

--- app/routes/Home.tsx
+++ app/routes/Home.tsx
@@ -25,15 +25,32 @@ export function Home() {
         }
     }, [])
 
+    const onCreateCode = () => {
+    }
+
     return (
         <div className={"flex flex:col"}>
-            <label className="text-align:center font:bold font:sans font-size:2em mb:0.5em" htmlFor="text">
-                Write any text
-            </label>
-            <input className="r:1rem b:1px|solid|#CCC px:1em py:0.3em font-size:1.2em mb:1em" type="text"
-                   onChange={({target}) => setValue(target.value)} value={value}/>
+            <div className={"flex flex:col bg:gray-90 pb:3em"}>
+                <div ref={canvasRef} className={"flex justify-content:center align-items:center w:100% py:1em"}/>
 
-            <div ref={canvasRef} style={{width: "100px", height: "100px", marginTop: "15px"}}></div>
+                <div className={"flex flex:col justify-content:center px:2em"}>
+                    <p className="text-align:center font:sans font-size:1em mt:3em mb:2em opacity:0.8">
+                        Ingresa o pega la URL del sitio web
+                    </p>
+
+                    <input className="r:1rem b:1px|solid|#CCC px:1em py:0.3em font-size:1.2em mb:2em" type="text"
+                           onChange={({target}) => setValue(target.value)} value={value}/>
+
+                    <button className={"r:1.5rem py:0.5em bg:black b:none color:white font:bold font-size:1.2em"}
+                            onClick={onCreateCode}>
+                        Crear código QR
+                    </button>
+                </div>
+            </div>
+
+            <div>
+
+            </div>
         </div>
     )
 }
